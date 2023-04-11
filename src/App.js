cdaafb1b4dd4b8d67006ae1762b09fd04aafbe73
“Marian <marianbreiman@gmail.com>
2023-04-11 20:08:16 -0300

    cambios solicitados

--- src/App.js
+++ src/App.js
@@ -7,24 +7,14 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <ItemListContainer 
+      bienvenida={bienvenida}
+      nombreDeMarca={nombreDeMarca}
+      < />
     </div>
   );
 }
 
-let bienvenida = "Bienvenido a la pagina";
-let nombreDeMarca = "Cloudy commerce";
-
-function Bienvenido() {
-  return (
-    <div className="App">
-      <Navbar />
-      <ItemListContainer
-        bienvenida={bienvenida}
-        nombreDeMarca={nombreDeMarca}
-      />
-    </div>
-  );
-}
 
 export default App;
 
